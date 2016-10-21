badccb530824832bdd5bbeb192f85b3a057e1a21
Nicolás Moral <nicomda@hotmail.com>
2016-10-21 03:31:12 +0200

    Updated instarun libraries due to an Android Studio Updated. Added SmoothProgressBar library gradle dependency Added SPB to device cards in main activity to show progress of background tasks Added TCPClient class that will give functionality to doors and switches.

---
+++ app/src/main/java/org/nicomda/pihome/device_actions/TCPClient.java
@@ -0,0 +1,14 @@
+package org.nicomda.pihome.device_actions;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by nicomda on 20/10/16.
+ */
+
+public class TCPClient extends AsyncTask<String, Integer, Void> {
+    @Override
+    protected Void doInBackground(String... params) {
+        return null;
+    }
+}
